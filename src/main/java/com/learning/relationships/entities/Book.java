cda31c167ecfb89a8c5a65bc0d5dc1e0a054e4b9
xandu1738 <xandu1738@gmail.com>
2023-03-28 14:49:30 +0300

    xtra

--- src/main/java/com/learning/relationships/entities/Book.java
+++ src/main/java/com/learning/relationships/entities/Book.java
@@ -30,4 +30,88 @@ public class Book {
     private Integer totalPages;
     private Double rating;
     private String publishedDate;
+
+    public Book(Long id, String title, Photo photo, Collection<Category> categories, Collection<Author> authors, Long isbn, Integer totalPages, Double rating, String publishedDate) {
+        this.id = id;
+        this.title = title;
+        this.photo = photo;
+        this.categories = categories;
+        this.authors = authors;
+        this.isbn = isbn;
+        this.totalPages = totalPages;
+        this.rating = rating;
+        this.publishedDate = publishedDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Photo getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(Photo photo) {
+        this.photo = photo;
+    }
+
+    public Collection<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Collection<Category> categories) {
+        this.categories = categories;
+    }
+
+    public Collection<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Collection<Author> authors) {
+        this.authors = authors;
+    }
+
+    public Long getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(Long isbn) {
+        this.isbn = isbn;
+    }
+
+    public Integer getTotalPages() {
+        return totalPages;
+    }
+
+    public void setTotalPages(Integer totalPages) {
+        this.totalPages = totalPages;
+    }
+
+    public Double getRating() {
+        return rating;
+    }
+
+    public void setRating(Double rating) {
+        this.rating = rating;
+    }
+
+    public String getPublishedDate() {
+        return publishedDate;
+    }
+
+    public void setPublishedDate(String publishedDate) {
+        this.publishedDate = publishedDate;
+    }
 }
--- src/main/java/com/learning/relationships/entities/Photo.java
+++ src/main/java/com/learning/relationships/entities/Photo.java
@@ -22,4 +22,52 @@ public class Photo {
     private String urlMedium;
     @Column(name = "url_large")
     private String urlLarge;
+
+    public Photo(Long id, Book book, String urlSmall, String urlMedium, String urlLarge) {
+        this.id = id;
+        this.book = book;
+        this.urlSmall = urlSmall;
+        this.urlMedium = urlMedium;
+        this.urlLarge = urlLarge;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public String getUrlSmall() {
+        return urlSmall;
+    }
+
+    public void setUrlSmall(String urlSmall) {
+        this.urlSmall = urlSmall;
+    }
+
+    public String getUrlMedium() {
+        return urlMedium;
+    }
+
+    public void setUrlMedium(String urlMedium) {
+        this.urlMedium = urlMedium;
+    }
+
+    public String getUrlLarge() {
+        return urlLarge;
+    }
+
+    public void setUrlLarge(String urlLarge) {
+        this.urlLarge = urlLarge;
+    }
 }
